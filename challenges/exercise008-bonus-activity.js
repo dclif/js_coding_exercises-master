7bd1c051a6853fde6b5b3ddbec2a9d3f622fdfb8
Daniel <65896341+dclif@users.noreply.github.com>
2022-02-10 12:28:10 +0000

    Update exercise008-bonus-activity.js
    exercise 8 all tests passed

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -3,22 +3,30 @@
 function printColours(colours){
     if (!colours) throw new Error("colour is required");
     // Your solution using the spread operator here!
+    return [...colours]
 }
 
 function shoppingList(list, newItem){
     if (!list) throw new Error("list is required");
     if (!newItem) throw new Error("newItem is required");
+    [...list, newItem] 
     // Your solution using the spread operator here!
+    return [...list, ...newItem]
 }
+
 
 function highestNumber(numbers){
     if (!numbers) throw new Error("numbers are required");
     // Your solution using the spread operator here!
+    return numbers.sort((a,b) => b-a)[0]
+
 }
+
 
 function splitThatString(string){
     if (!string) throw new Error("string is required");
     // Your solution using the spread operator here!
+    return [...string]
 }
 
 
@@ -27,7 +35,11 @@ function splitThatString(string){
 function addressLookUp(user){
     if (!user) throw new Error("user is required");
     // Your solution using optional chaining here!
+    // eslint-disable-next-line no-prototype-builtins
+    return user.hasOwnProperty("address") ? user.address.postcode : undefined;
 }
+
+
 
 
 module.exports = {
